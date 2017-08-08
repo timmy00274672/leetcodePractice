5e2eba0e950eb9cbde4a68844ece7c38b138b7db
timmy00274672 <timmy00274672@gmail.com>
2017-08-08 14:01:53 +0800

    finish Medium 11

--- src/main/java/leetcode/medium/Medium11.java
+++ src/main/java/leetcode/medium/Medium11.java
@@ -3,21 +3,21 @@ package leetcode.medium;
 public class Medium11 {
 
 	public static void main(String[] args) {
-
+		System.out.println(maxArea(new int[] { 1, 1, 3, 5, 4, 6, 7, 1, 2, 3, 4 }));
 	}
 
 	public static int maxArea(int[] height) {
 		int maxWater = -1;
-		for (int left = 0; left < height.length; left++) {
-			int rightHeightForMax = -1;
-			for (int right = height.length - 1; right > left; right--) {
-				if (height[right] > rightHeightForMax) {
-					int water = Math.min(height[left], height[right]) * (right - left);
-					if (water > maxWater) {
-						maxWater = water;
-						rightHeightForMax = height[right];
-					}
-				}
+		int left = 0, right = height.length - 1;
+		while (left < right) {
+			int water = Math.min(height[left], height[right]) * (right - left);
+			if (water > maxWater) {
+				maxWater = water;
+			}
+			if (height[left] > height[right]) {
+				right--;
+			} else {
+				left++;
 			}
 		}
 		return maxWater;
